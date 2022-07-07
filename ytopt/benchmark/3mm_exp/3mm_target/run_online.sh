0e64603b2a33273ac000704057f9040b1b3eaa2d
Jaehoon Koo <jkoo@gpulogin2.lcrc.anl.gov>
2022-07-06 20:08:12 -0500

    polybench seeds

--- ytopt/benchmark/3mm_exp/3mm_target/run_online.sh
+++ ytopt/benchmark/3mm_exp/3mm_target/run_online.sh
@@ -4,34 +4,59 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --output=stdout_sdv_run1.%j
+#SBATCH --output=stdout_sdv_run1_seed.%j
 
 # module load nvhpc/21.5-oxhtyof
 source /soft/anaconda3/2020.02/etc/profile.d/conda.sh
 source activate /home/jkoo/.conda/envs/ytune
 cd /lcrc/project/EE-ECP/jkoo/code/ytopt/ytopt/benchmark/3mm_exp/3mm_target/
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_sm
-mv results_sdv.csv results_sdv_sm_3mm.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_3mm_2022.csv
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_ml
-mv results_sdv.csv results_sdv_ml_3mm.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_3mm_2022.csv
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_xl
-mv results_sdv.csv results_sdv_xl_3mm.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_3mm_2022.csv
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_sm
-mv results_sdv.csv results_sdv_sm_3mm_refit.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_3mm_refit_2022.csv
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_ml
-mv results_sdv.csv results_sdv_ml_3mm_refit.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_3mm_refit_2022.csv
 # ##############
-python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800
-mv tmp_files tmp_files_xl
-mv results_sdv.csv results_sdv_xl_3mm_refit.csv
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 2022
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_3mm_refit_2022.csv
+# ##############
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_3mm_9999.csv
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_3mm_9999.csv
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 30 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_3mm_9999.csv
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target sm -itarget 110 120 130 140 150 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_3mm_refit_9999.csv
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target ml -itarget 490 545 600 655 710 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_3mm_refit_9999.csv
+# ##############
+python Run_online_TL.py --kernel_name 3mm --max_evals 30 --n_refit 5 --top 0.3 --nparam 10  --param_start 0 --target xl -itarget 1600 1800 2000 2200 2400 -imin 16 18 20 22 24 -imax 3200 3600 4000 4400 4800 --seed 9999
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_3mm_refit_9999.csv
 # ##############
--- ytopt/benchmark/covariance_exp/covariance_target/run_online.sh
+++ ytopt/benchmark/covariance_exp/covariance_target/run_online.sh
@@ -4,68 +4,60 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --output=stdout_sdv_run1.%j
+#SBATCH --output=stdout_sdv_run1_seed.%j
 
 # module load nvhpc/21.5-oxhtyof
 source /soft/anaconda3/2020.02/etc/profile.d/conda.sh
 source activate /home/jkoo/.conda/envs/ytune
 cd /lcrc/project/EE-ECP/jkoo/code/ytopt/ytopt/benchmark/covariance_exp/covariance_target/
-# ##############
-# python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000
-# mv tmp_files tmp_files_sm
-# mv results_sdv.csv results_sdv_sm_covariance.csv
-# # ##############
-# python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000
-# mv tmp_files tmp_files_ml
-# mv results_sdv.csv results_sdv_ml_covariance.csv
-# # ##############
-# python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000
-# mv tmp_files tmp_files_xl
-# mv results_sdv.csv results_sdv_xl_covariance.csv
-# ##############
-# python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000
-# mv tmp_files tmp_files_sm_refit
-# mv results_sdv.csv results_sdv_sm_covariance_refit.csv
-# ##############
-python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000
-mv tmp_files tmp_files_ml_refit
-mv results_sdv.csv results_sdv_ml_covariance_refit_re.csv
-# ##############
-python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000
-mv tmp_files tmp_files_xl_refit
-mv results_sdv.csv results_sdv_xl_covariance_refit_re.csv
-# mkdir ./tmp_results
-# rm ytopt.log
-# python -m ytopt.search.ambs --evaluator subprocess --problem problem_all.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge --set-SEED 2468 --set-NI 10
-# mv results.csv results_rf_ml_xsbench.csv
-# mv tmp_results tmp_results_rf_run1
-# mv tmp_files tmp_files_rf_run1
-# mv ytopt.log ytopt_rf_ml_xsbench_run1.log
-# ##############
-# rm ytopt.log
-# mkdir ./tmp_results
-# python -m ytopt.search.ambs --evaluator subprocess --problem problem_all.Problem --max-evals=200 --learner DUMMY --set-KAPPA 1.96 --acq-func gp_hedge --set-SEED 2468 --set-NI 10
-# mv results.csv results_rs_l_covariance.csv
-# mv tmp_results tmp_results_rs_run1
-# mv tmp_files tmp_files_rs_run1
-# mv ytopt.log ytopt_rs_l_covariance_run1.log
-# # ##############
-# rm ytopt.log
-# mkdir ./tmp_results
-# python -m ytopt.search.ambs --evaluator subprocess --problem problem_all.Problem --max-evals=200 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge --set-SEED 2468 --set-NI 10
-# mv results.csv results_rf_l_covariance.csv
-# mv tmp_results tmp_results_rf_run1
-# mv tmp_files tmp_files_rf_run1
-# mv ytopt.log ytopt_rf_l_covariance_run1.log
 ##############
-# mkdir ./tmp_results
-# python learnBO_1_sdv_itr.py
-# mv results_sdv_itr.csv results_sdv_itr_ml_xsbench.csv
-# mv tmp_results tmp_results_sdv_itr_run1
-# mv tmp_files tmp_files_sdv_itr_run1
-#############
-# mkdir ./tmp_results
-# python learnBO_1_sdv.py
-# mv results_sdv.csv results_sdv_ml_xsbench.csv
-# mv tmp_results tmp_results_sdv_run1
-# mv tmp_files tmp_files_sdv_run1
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_covariance_2022.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_covariance_2022.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_covariance_2022.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_sm_refit
+mv results_sdv.csv results_sdv_sm_covariance_refit_2022.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_ml_refit
+mv results_sdv.csv results_sdv_ml_covariance_refit_2022.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000 --seed 2022
+# mv tmp_files tmp_files_xl_refit
+mv results_sdv.csv results_sdv_xl_covariance_refit_2022.csv
+##############
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_sm
+mv results_sdv.csv results_sdv_sm_covariance_9999.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_ml
+mv results_sdv.csv results_sdv_ml_covariance_9999.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_xl
+mv results_sdv.csv results_sdv_xl_covariance_9999.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 160 180 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_sm_refit
+mv results_sdv.csv results_sdv_sm_covariance_refit_9999.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 720 830 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_ml_refit
+mv results_sdv.csv results_sdv_ml_covariance_refit_9999.csv
+##############
+python Run_online_TL.py --kernel_name covariance --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 2600 3000 -imin 28 32 -imax 3800 4000 --seed 9999
+# mv tmp_files tmp_files_xl_refit
+mv results_sdv.csv results_sdv_xl_covariance_refit_9999.csv
+##############
+
