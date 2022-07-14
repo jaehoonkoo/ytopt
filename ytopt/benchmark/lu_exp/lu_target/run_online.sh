82e7572ca554372fe8b3ded7fa4b83d6805fd0e0
Jaehoon Koo <jkoo@gpulogin1.lcrc.anl.gov>
2022-07-14 16:58:18 -0500

    polybench seed 0714

--- ytopt/benchmark/lu_exp/lu_target/run_online.sh
+++ ytopt/benchmark/lu_exp/lu_target/run_online.sh
@@ -23,15 +23,15 @@ python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 -
 mv tmp_files tmp_files_xl
 mv results_sdv.csv results_sdv_xl_lu_2022.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 260 -imin 40 -imax 6000 --seed 2022
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 260 -imin 40 -imax 6000 --seed 2022
 mv tmp_files tmp_files_sm_refit
 mv results_sdv.csv results_sdv_sm_lu_refit_2022.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 1200 -imin 40 -imax 6000 --seed 2022
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 1200 -imin 40 -imax 6000 --seed 2022
 mv tmp_files tmp_files_ml_refit
 mv results_sdv.csv results_sdv_ml_lu_refit_2022.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 4000 -imin 40 -imax 6000 --seed 2022
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 4000 -imin 40 -imax 6000 --seed 2022
 mv tmp_files tmp_files_xl_refit
 mv results_sdv.csv results_sdv_xl_lu_refit_2022.csv
 ##############
@@ -48,14 +48,14 @@ python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 -
 mv tmp_files tmp_files_xl
 mv results_sdv.csv results_sdv_xl_lu_9999.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 260 -imin 40 -imax 6000 --seed 9999
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target sm -itarget 260 -imin 40 -imax 6000 --seed 9999
 mv tmp_files tmp_files_sm_refit
 mv results_sdv.csv results_sdv_sm_lu_refit_9999.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 1200 -imin 40 -imax 6000 --seed 9999
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target ml -itarget 1200 -imin 40 -imax 6000 --seed 9999
 mv tmp_files tmp_files_ml_refit
 mv results_sdv.csv results_sdv_ml_lu_refit_9999.csv
 ##############
-python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 30 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 4000 -imin 40 -imax 6000 --seed 9999
+python Run_online_TL.py --kernel_name lu --max_evals 30 --n_refit 5 --top 0.3 --nparam 5 --param_start 1 --target xl -itarget 4000 -imin 40 -imax 6000 --seed 9999
 mv tmp_files tmp_files_xl_refit
 mv results_sdv.csv results_sdv_xl_lu_refit_9999.csv
